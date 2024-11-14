6809699e62eff39c571e60ab45f98d44e658419e
AbdielP <abdiel.pinzonc@gmail.com>
2024-11-13 20:32:45 -0500

    sx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { Button } from "@mui/material"
+import { Button, Container } from "@mui/material"
 
 function App() {
   return (
-    <>
+    <Container sx={{
+      border: '2px solid red', boxShadow: 3, pb: 2
+    }}>
       <h1>App</h1>
       <Button variant="contained">Botón</Button>
-    </>
+    </Container>
   )
 }
 
