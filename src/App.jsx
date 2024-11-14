f1fee9e130e95b44cb217e2202d3a760015d14ce
AbdielP <abdiel.pinzonc@gmail.com>
2024-11-13 20:04:04 -0500

    probando MUI

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material"
+
+function App() {
+  return (
+    <>
+      <h1>App</h1>
+      <Button variant="contained">Botón</Button>
+    </>
+  )
+}
+
+export default App
